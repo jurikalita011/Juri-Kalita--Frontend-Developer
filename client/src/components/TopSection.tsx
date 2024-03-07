564f529ae9b9701a6b373a59842c9195e2536ed7
Juri Kalita <jurikalita011@gmail.com>
2024-03-07 18:21:31 +0530

    added bg for sm screen

--- client/src/components/TopSection.tsx
+++ client/src/components/TopSection.tsx
@@ -3,12 +3,14 @@ import { Navbar } from "./Navbar";
 
 export const TopSection = () => {
   return (
-    <div className="w-full h-[36.75rem] top-bg abolute">
+    <div className="w-full h-[36.75rem] top-bg">
       <Navbar />
       <div className="flex justify-center items-center absolute">
-        <p className="w-[53%] lg:w-[83.4%] text-center text-[1.3rem] lg:text-[2.8125rem] leading-[1.6rem] lg:leading-[2.8125rem] text-[#FBF6F6] relative top-[9.375rem] shojumaru-regular">
-          Where Blockchain Heroes Thrive, Rescuing Dreams, Elevating Fortunes.
-        </p>
+        <div className="w-[85%] py-4 flex justify-center items-center bg-[#24232366] md:bg-transparent lg:bg-transparent rounded-[20px] md:rounded-none lg:rounded-none lg:w-[83.4%] relative top-[6.2rem] md:top-[9.375rem] lg:top-[9.375rem]">
+          <p className="w-[70%] lg:w-full text-center text-[1.3rem] lg:text-[2.8125rem] leading-[1.6rem] lg:leading-[2.8125rem] text-[#FBF6F6] shojumaru-regular">
+            Where Blockchain Heroes Thrive, Rescuing Dreams, Elevating Fortunes.
+          </p>
+        </div>
       </div>
     </div>
   );
