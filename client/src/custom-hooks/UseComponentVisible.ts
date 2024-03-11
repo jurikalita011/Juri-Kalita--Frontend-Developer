801c6c3a6d3132e31f893022232b946980fd5f92
Juri Kalita <jurikalita011@gmail.com>
2024-03-11 13:21:12 +0530

    fixed visibility

--- client/src/custom-hooks/UseComponentVisible.ts
+++ client/src/custom-hooks/UseComponentVisible.ts
@@ -8,8 +8,8 @@ export const useComponentVisible = () => {
     const handleScroll = () => {
       if (componentRef.current) {
         const rect = componentRef.current.getBoundingClientRect();
-        const isInView = rect.top <= window.innerHeight / 2;
-        // rect.bottom >= window.innerHeight / 2;
+        const isInView = rect.top <= (2 * window.innerHeight) / 3;
+
         setIsVisible(isInView);
       }
     };
